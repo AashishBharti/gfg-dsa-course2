145837638a2f764d6400be7f0875d89ec6202324
AashishBharti <aashishbharti6@gmail.com>
2023-02-19 16:54:55 +0530

    Frequency_Count
    Frequency_Count

---
+++ Frequency_Count.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void frequencyCount(int arr[],int n)
+{
+    int freq=1,i=1;
+    while(i<n)
+    {
+        while(i<n && arr[i] ==arr[i-1])
+        {
+            freq++;
+            i++;
+        }
+        cout<<arr[i-1]<<" "<<freq<<endl;
+
+        i++;
+        freq=1;
+
+    }
+    if(n==1 || arr[n-1]!=arr[n-2])
+    {
+        cout<<arr[n-1]+" "+1;
+    }
+
+}
+
+int main()
+{
+	
+      int arr[] = {10, 10, 20, 30, 30, 30}, n = 6;
+
+      frequencyCount(arr, n);
+    
+}
+
