950ddeb3110991e09bf024b50bc264b11d130630
AashishBharti <aashishbharti6@gmail.com>
2023-02-26 13:29:18 +0530

    Buy_And_Sell

---
+++ Buy_And_Sell.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProfit(int price[],int start,int end )
+{
+    int profit=0;
+    if(start >=end)
+    {
+        return 0;
+    }
+    for(int i=start;i<end;i++)
+    {
+        for(int j=i+1;j<=end;j++)
+        {
+            if(price[j]>price[i])
+            {
+                int curr_sum=price[j]-price[i]+maxProfit(price,start,i-1)+maxProfit(price,j+1,end);
+                profit= max(profit,curr_sum);
+            }
+            
+            
+        }
+    }
+    return profit;
+
+}
+int main()
+{
+    int arr[] = {1, 5, 3, 8, 12}, n = 5;
+
+      cout<<maxProfit(arr, 0, n - 1);
+}
