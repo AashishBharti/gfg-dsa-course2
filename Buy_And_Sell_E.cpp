87279f0bbc999de3792fa3349089970f6d78185f
AashishBharti <aashishbharti6@gmail.com>
2023-03-05 19:21:35 +0530

    Practice

---
+++ Buy_And_Sell_E.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int maxprofit(int arr[],int n)
+{
+    int profit=0;
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]>arr[i-1])
+        {
+            profit+=arr[i]-arr[i-1];
+        }
+    }
+    return profit;
+}
+
+int main()
+{
+    int arr[] = {1, 5, 3, 8, 12}, n = 5;
+    cout<<maxprofit(arr,n);
+}
