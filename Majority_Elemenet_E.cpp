6a9173bd32ae53cd685023424127135682a8a988
AashishBharti <aashishbharti6@gmail.com>
2023-03-11 02:43:36 +0530

    practice

---
+++ Majority_Elemenet_E.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int majority(int arr[],int n)
+{
+    int res=0;int count=1;
+    for(int i=1;i<n;i++)
+    {
+        if(arr[res]==arr[i])
+        {
+            count++;
+        }
+        else
+        {
+            count--;
+        }
+        if(count==0)
+        {
+            res=i;
+            count=1;
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(arr[res]==arr[i])
+        {
+            count++;
+        }
+
+    }
+    if(count<=n/2)
+    {
+        return -1;
+    }
+    return res;
+}
+int main()
+{
+int arr[] = {8, 8, 6, 6, 6, 4, 6}, n = 7;
+
+     cout<<majority(arr, n);
+}
