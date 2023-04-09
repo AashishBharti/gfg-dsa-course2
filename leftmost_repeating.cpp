e362ad3947a54bcae0afd00fa5688bc00f7f573f
AashishBharti <aashishbharti6@gmail.com>
2023-04-09 14:00:05 +0530

    practice

---
+++ leftmost_repeating.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int leftmost(string str)
+{
+    for(int i=0;i<str.length();i++)
+    {
+        for(int j=i+1;j<str.length();j++)
+        {
+            if(str[i]==str[j])
+            {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    string str;
+    cin>>str;
+    cout<<leftmost(str);
+
+}
