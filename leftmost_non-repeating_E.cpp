b0fbe64aa93252bd68e39e561091f3360602ab20
AashishBharti <aashishbharti6@gmail.com>
2023-04-11 23:23:31 +0530

    practice

---
+++ leftmost_non-repeating_E.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int CHAR=256;
+int leftmost(string str)
+{
+    int count[CHAR]={0};
+   for(int i=0;i<str.length();i++)
+   {
+        count[str[i]]++;
+   }
+   for(int i=0;i<str.length();i++)
+   {
+    if(count[str[i]]==1)
+    {
+        return i;
+    }
+   }
+   return -1;
+}
+
+int main()
+{
+    string str;
+    cin>>str;
+    cout<<leftmost(str);
+}
