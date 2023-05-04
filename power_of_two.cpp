db54a71e1d588892548aba48b10dbceb44041250
AashishBharti <aashishbharti6@gmail.com>
2023-05-04 12:48:00 +0530

    power of two

--- power_of_two.cpp
+++ power_of_two.cpp
@@ -1,27 +1,32 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-bool ispow_2(int n)
-{
-    if(n==0)
-    {
-        return false;
-    }
-    while(n>1)
-    {
-        if(n%2!=0)
-        {
-            return false;
-        }
-        n=n/2;
-    }
-    return true;
 
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>a[i];
+	    }
+	    int res=a[0];
+	    int maxending=a[0];
+	    
+	   for(int i=1;i<n;i++)
+	   {
+	       maxending=max(maxending+a[i],a[i]);
+	       res=max(res,maxending);
+	       
+	   }
+	   cout<<res-1<<endl;
+	
+	return 0;
 }
-
-int main()
-{
-    int n=6;
-    cout<<ispow_2(n);
 }
